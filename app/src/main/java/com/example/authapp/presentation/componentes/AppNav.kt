a53e8d695362764eba3981e3374ae54980429536
EdgarSR-1 <A01286059@tec.mx>
2025-10-12 20:16:19 -0600

    Inicio AppNav

--- app/src/main/java/com/example/authapp/presentation/componentes/AppNav.kt
+++ app/src/main/java/com/example/authapp/presentation/componentes/AppNav.kt
@@ -1,2 +1,31 @@
 package com.example.authapp.presentation.componentes
 
+import android.net.Uri
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.compose.runtime.getValue
+import androidx.navigation.NavType
+import androidx.navigation.navArgument
+import androidx.core.net.toUri
+
+import com.example.authapp.presentation.mainViews.HomeScreen
+
+object Routes {
+    const val HOME = "mainScreen"
+}
+
+@Composable
+fun AppNav( // en instacam era viewModel: GalleryViewModel
+) {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = Routes.HOME
+    ) {
+
+    }
+}
